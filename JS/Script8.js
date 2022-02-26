356f2a3ca18e4ef637d77a0145366dce6ec319f3
CristianCosticea <97525096+CristianCosticea@users.noreply.github.com>
2022-02-26 14:16:17 +0200

    Add files via upload

---
+++ JS/Script8.js
@@ -0,0 +1,15 @@
+const EventEmmiter = require('events');
+
+var url = 'http://mylogger.io/log';
+
+class Logger extends EventEmmiter {
+    log(message) {
+        // Send an HTTP request
+        console.log(message);
+
+        //Raise an event
+        this.emit('messageLogged', { id: 1, url: 'http://' }};
+}  
+}  
+
+module.exports = Logger;
